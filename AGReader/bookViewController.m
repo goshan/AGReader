5fcafd458f96a75597d79cb3e602d0fc5c63b9d5
goshan <goshan.hanqiu@gmail.com>
2012-09-18 16:37:00 +0800

    added icon and remove orignal

--- AGReader/bookViewController.m
+++ AGReader/bookViewController.m
@@ -222,15 +222,15 @@ float leftInsets = 20.0;
 	[super viewDidDisappear:animated];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
-}
+//- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+//{
+//    // Return YES for supported orientations
+//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+//    } else {
+//        return YES;
+//    }
+//}
 
 - (void)dealloc {
     [_content release];
